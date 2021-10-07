4d3a2dbb67d418562a13d09b6143b20c64715ae3
Nikolaj Baer <nikolaj.baer@gmail.com>
2021-10-06 18:50:41 -0700

    inital commit of chart and map

--- main.js
+++ main.js
@@ -1,25 +1,34 @@
 import './style.css'
 
-import * as duration from "iso8601-duration"
+import * as duration from 'iso8601-duration'
 import * as d3 from "d3"
 import * as SunCalc from "suncalc"
-// moment
+import * as moment from "moment"
+import * as L from "leaflet"
+import "leaflet/dist/leaflet.css"
+
+import { chart } from "./src/chart"
 
 function init(){
-  const el = document.getElementById("app")
   const latlng = {lat:32.7499568,lng:-117.2521772}
   load_forecast(latlng).then( result => {
     console.log(result)
+    window.data = result
     return result
   }).then( result =>{
-    el.innerHTML = `
-      <h1>Hello World!</h1>
-      ${result.name}
-    `
+    const days_data = days(result.forecast,latlng)
+    const hourly_data = hourly(result.forecast)
+    console.log(days_data,hourly_data)
+    document.getElementById("app").appendChild(
+      chart(days_data,hourly_data)
+    )
+    document.getElementById("app").appendChild(
+      map(latlng,result)
+    )
   })
 }
 
-function browseR_location(){
+function browser_location(){
   return new Promise(function (resolve, reject) {
     navigator.geolocation.getCurrentPosition(resolve, reject, {  
       enableHighAccuracy: true,
@@ -29,15 +38,149 @@ function browseR_location(){
   })
 }
 
+// utility to expand the iso date timespan formats, e.g. PT6H, to 6 hourly values
+// note maybe we need to pass in the extent we want as well
+function expand_forecast(values,f){
+  const data = []
+  values.forEach( d => {
+    const values = d.validTime.split('/');
+    const t = Date.parse(values[0]);
+    const td = duration.parse(d.validTime)
+    const hours = td.hours + (td.days*24)
+    for(var i=0;i<hours;i++){
+      const t1 = new Date(t + 60*60*1000*i)
+      data.push({  
+        gmtTime: t1.toGMTString(), 
+        time: t1,
+        value: f!=null?f(d):d,
+      })
+    }
+  })
+  return data;
+}
+
+
 
 function load_forecast(latlng){
   return fetch(`https://api.weather.gov/points/${latlng.lat},${latlng.lng}`).then( response => response.json())
     .then( data => {
-      return fetch(data.properties.forecastOffice)
-    }).then( response => response.json() )
+      return Promise.all([
+        fetch(data.properties.forecastOffice).then( response => response.json() ),
+        fetch(data.properties.forecastGridData).then( response => response.json() ),
+        new Promise( (resolve,reject) => resolve(data)),
+      ])
+    })
+    .then( data => {
+      return {station:data[0],forecast:data[1],location:data[2]}
+    })
     .catch(err => {
       console.error('Request failed', err)
     })
 }
 
+const metrics = [
+  ['temperature', d => d.value * 9/5 + 32],
+  ['dewpoint', d => d.value * 9/5 + 32],
+  ['relativeHumidity', d => d.value],
+  ['probabilityOfPrecipitation', d => d.value],
+  ['skyCover', d => d.value],
+  ['windSpeed', d => d.value],
+  ['windDirection', d => d.value],
+  ['pressure', d => d.value],
+]
+
+
+// TODO Tides https://api.tidesandcurrents.noaa.gov/api/prod/
+function days(forecast,latlng){
+  const days = [];
+  const forecast_start = moment.parseZone(forecast.properties.validTimes.split('/')[0]).toDate()
+  console.log("Forecast Valid for ",forecast.properties.validTimes,forecast_start,duration.parse(forecast.properties.validTimes))
+
+  //const start_date = new Date(forecast_start.getFullYear(),forecast_start.getMonth(),forecast_start.getDate()+1)
+  
+  const weather_values = expand_forecast(forecast.properties.weather.values)
+  const min_temps = expand_forecast(forecast.properties.minTemperature.values)
+  const max_temps = expand_forecast(forecast.properties.minTemperature.values)
+  const precip_total = expand_forecast(forecast.properties.quantitativePrecipitation.values)
+  // Todo expand out a forecast so we can pull the value for a specific time / date.
+
+  // get local timezone offset
+  // All incoming times are in UTC +00, but we want to show local time per
+  // user's browser
+  const offset = new Date().getTimezoneOffset()
+  console.log("Starts at",forecast_start)
+
+  // build a list of our 8 days, start and end, noon, and sun times
+  var j = 0
+  for(var i=0;i<8;i++){
+    const start = moment(forecast_start).add(i,'days')
+    const noon = start.startOf('day').add(12,'hours').toDate()
+    
+    const day_start = start.startOf('day').toDate()
+    const inclement = forecast_for_time("weather",noon,forecast)
+    const precip_total = forecast_for_time("quantitativePrecipitation",noon,forecast).value.value * 0.393701 * .1 // inches
+    const skyCover = forecast_for_time("skyCover",noon,forecast).value.value
+
+    
+    const day = {
+      start: start.startOf('day').toDate(),
+      end: start.endOf('day').toDate(),
+      noon: noon,
+      inclement: inclement,
+      skyCover: skyCover,
+      temp:{
+        min:forecast_for_time("minTemperature",day_start,forecast).value.value,
+        max:forecast_for_time("maxTemperature",day_start,forecast).value.value,
+      },
+      sun:SunCalc.getTimes(noon, latlng.lat,latlng.lng),
+      precip_total: precip_total,
+    }
+    days.push(day)
+  }
+  return days; //,weather_values]
+}
+
+function forecast_for_time(metric,d,forecast){
+  const values = expand_forecast(forecast.properties[metric].values)
+  const matching = values.filter(v => v.time >= d)
+  if(matching.length ==0){ return values[0] }
+  return matching[0]
+}
+
+function hourly(forecast){
+  const pt_regex = /\d+/g
+  const data = {}
+  metrics.forEach( metric_pak => {
+    data[metric_pak[0]] = expand_forecast( forecast.properties[metric_pak[0]].values, metric_pak[1] )
+  })
+  return data;
+}
+
+
+function map(latlng,data){
+  let container = document.createElement("div")
+  container.style.height = "400px"
+  let map = L.map(container).setView([latlng.lat,latlng.lng], 14);
+  let osmLayer = L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}@2x.png', {
+      attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+  }).addTo(map);
+  L.marker([latlng.lat,latlng.lng]).addTo(map)
+    .bindPopup(data.station.name);
+  L.geoJSON([data.forecast], {
+		style: function (feature) {
+			return feature.properties && feature.properties.style;
+		},
+    pointToLayer: function (feature, latlng) {
+			return L.circleMarker(latlng, {
+				radius: 8,
+				fillColor: "#ff7800",
+				color: "#000",
+				weight: 1,
+				opacity: 1,
+				fillOpacity: 0.8
+			});
+		}
+	}).addTo(map);
+  return container
+}
 init()
--- src/d3_elements.js
+++ src/d3_elements.js
@@ -1,27 +1,35 @@
 import * as d3 from "d3"
+import * as moment from "moment"
 
-class DaySummary {
+function svg(contents){
+  const e = document.createElement("svg")
+  e.innerHTML = contents
+  return e
+}
+const formatDate = d3.timeFormat("%a %b %-d, %I%p")
+
+export class DaySummary {
   constructor(day) {
     const wv = determine_weather(day);
    
-    this.node = svg`<g font-family="sans-serif" font-size="12" text-anchor="middle">
+    this.node = svg(`<g font-family="sans-serif" font-size="12" text-anchor="middle">
         <text y="0">${moment(day.noon).format('ddd M/D')}</text>
         <text y="14">${formatTemp(day.temp.max)} | ${formatTemp(day.temp.min)}</text>
         <text y="28">${wv.n} ${wv.i}</text>
         <text y="42">${ day.precip_total.toFixed(1) }" ${(day.precip_total>0?'💧':'')}</text>
-    </g>`;
+    </g>`);
   }
 }
 
-class VertLine {
+export class VertLine {
   constructor(y0,y1,stroke_color) {
-    this._pos = svg`<text x="4" y="${y0} - 12"></text>`;
-    this._line = svg`<path d="M 0 ${y0} L 0 ${y1}" stroke="${stroke_color}">`;
-    this._points = svg`<g></g>`;
-    this.node = svg`<g pointer-events="none" display="none" font-family="sans-serif" font-size="10" text-anchor="middle">
+    this._pos = svg(`<text x="4" y="${y0} - 12"></text>`);
+    this._line = svg(`<path d="M 0 ${y0} L 0 ${y1}" stroke="${stroke_color}">`);
+    this._points = svg(`<g></g>`);
+    this.node = svg(`<g pointer-events="none" display="none" font-family="sans-serif" font-size="10" text-anchor="middle">
       ${this._pos}
       ${this._line}
-    </g>`;
+    </g>`);
   }
   show(x,y,t,points) {
     this.node.removeAttribute("display");
@@ -41,98 +49,18 @@ class VertLine {
   }
 }
 
-class WindArrow {
+export class WindArrow {
   constructor(fill_color) {
-    this.node = svg`<polygon points="0, -5 -3, 3 0, 1 3, 3" fill="${fill_color}">`;
+    this.node = svg(`<polygon points="0, -5 -3, 3 0, 1 3, 3" fill="${fill_color}">`);
   }
 }
 
-const wind_line = d3.line()
-    .curve(d3.curveCatmullRom)
-    .x( d => x(d.time) )
-    .y(d => windspeed_y(d.value * 0.621371) ) // convert kmph to mph
 
-const windspeed_yAxis =  g => g
-    .attr("transform", `translate(${margin.left},0)`)
-    .call(d3.axisLeft(windspeed_y))
-    .call(g => g.select(".domain").remove())
-    .call(g => g.selectAll(".tick line").clone()
-        .attr("x2", width)
-        .attr("stroke-opacity", 0.1))
-    .call(g => g.append("text")
-        .attr("x", -margin.left)
-        .attr("y", 10)
-        .attr("fill", "currentColor")
-        .attr("text-anchor", "start"))
-
-const windspeed_y = d3.scaleLinear()
-    .domain([0,d3.max(hourly.windSpeed,d => d.value * 0.621371)]).nice() // kmph to mph
-    .range([height/2 - margin.bottom, margin.top])
-
-const precip_yAxis = g => g
-    .attr("transform", `translate(${margin.left},0)`)
-    .call(d3.axisLeft(precip_y))
-    .call(g => g.select(".domain").remove())
-    .call(g => g.selectAll(".tick line").clone()
-        .attr("x2", width)
-        .attr("stroke-opacity", 0.1))
-    .call(g => g.append("text")
-        .attr("x", -margin.left)
-        .attr("y", 10)
-        .attr("fill", "currentColor")
-        .attr("text-anchor", "start"))
-
-const yAxis = g => g
-    .attr("transform", `translate(${margin.left},0)`)
-    .call(d3.axisLeft(y))
-    .call(g => g.select(".domain").remove())
-    .call(g => g.selectAll(".tick line").clone()
-        .attr("x2", width)
-        .attr("stroke-opacity", 0.1))
-    .call(g => g.append("text")
-        .attr("x", -margin.left)
-        .attr("y", 10)
-        .attr("fill", "currentColor")
-        .attr("text-anchor", "start"))
-
-const precip_y = d3.scaleLinear()
-    .domain([0,100]).nice()
-    .range([height - margin.bottom, margin.top])
-
-const y = d3.scaleLinear()
-    .domain(d3.extent(d3.merge([hourly.dewpoint,hourly.temperature]), d => d.value)).nice()
-    .range([height - margin.bottom, margin.top])
-
-const x = d3.scaleTime()
-    .domain(day_extent)
-    .range([margin.left, width - margin.right])
-    .clamp(true)
-
-function day_extent(){
-  const days = d3.extent(hourly.temperature, d => d.time)
-  return [moment(days[0]).startOf('day').toDate(),moment(days[1]).endOf('day').toDate()]
+export function determine_weather(weather){
+  if(weather.precip_total > 0){ return {n:"Rainy",i:"☔"} }  // todo drizzle, rain, snow
+  if(weather.skyCover > 50){ return {n:"Cloudy",i:"☁️"}; } 
+  if(weather.skyCover > 10){ return {n:"Partly Cloudy",i:"⛅"}; }
+  return {n:"Sunny",i:"☀️"};
 }
 
-const xAxis = g => g
-    .attr("transform", `translate(0,${height - margin.bottom})`)
-    .call(d3.axisBottom(x).ticks(d3.timeHour.every(12)))
-    .call(g => g.select(".domain").remove())
-    .call(g => g.selectAll(".tick line").clone()
-        .attr("y2", -height)
-        .attr("stroke-opacity", 0.1))
-
-const precip_line = d3.line()
-    .x( d => x(d.time) )
-    .y(d => precip_y(d.value) )
-
-const line = d3.line()
-    .curve(d3.curveCatmullRom)
-    .x( d => x(d.time) )
-    .y(d => y(d.value) )
-
-const dawn_time_blocks = d3.area()
-  .x0( d => x(d.start) )
-  .x1( d => x(d.sunrise) )
-  .y0(0)
-  .y1(height)
-
+function formatTemp(C){ return (Math.round((C*(9/5)) + 32) + "°F") }
